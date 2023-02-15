e3f469ea000cb1c78518faa42822a1ece0c9970c
rostom <rostom.chettaoui@esprit.tn>
2023-02-16 00:44:19 +0100

    dorm-management

---
+++ src/main/java/tn/esprit/pidev4sae2back/controllers/FoyerController.java
@@ -0,0 +1,16 @@
+package tn.esprit.pidev4sae2back.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FoyerController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
